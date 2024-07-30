808f98958cb1fa52bd1e9de1c697600635758c0a
Divyesh1692 <divyeshvariya1692@gmail.com>
2024-07-30 21:22:59 +0530

    Node API

--- API/index.js
+++ API/index.js
@@ -2,8 +2,11 @@ const { log } = require("console");
 const express = require("express");
 const app = express();
 app.listen("2000", () => {
-  console.log("running");
+  console.log("Listening");
 });
 app.get("/api1", (req, res) => {
-  res.send("hello");
+  res.send("<h1>Hello API 1</h1>");
+});
+app.get("/api2", (req, res) => {
+  res.send("<h1>Hellp API 2</h1>");
 });
