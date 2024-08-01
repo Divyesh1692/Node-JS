fb609b31b78341d3a2e83bdc110c2429097300b1
Divyesh1692 <divyeshvariya1692@gmail.com>
2024-08-01 20:22:26 +0530

    Node JS CRUD

--- DB/script.js
+++ DB/script.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dbConnect = require("./db");
 const User = require("./user.schema");
+const isValid = require("./validation");
 
 const app = express();
 
@@ -11,9 +12,21 @@ app.get("/get", async (req, res) => {
   res.send(data);
 });
 
-app.post("/post", async (req, res) => {
+app.post("/post", isValid, async (req, res) => {
   let data = await User.create(req.body);
   console.log(req.body);
+  res.send(data);
+});
+
+app.patch("/patch/:id", async (req, res) => {
+  let { id } = req.params;
+  let data = await User.findByIdAndUpdate(id, req.body);
+  res.send(data);
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  let { id } = req.params;
+  let data = await User.findByIdAndDelete(id);
   res.send(data);
 });
 
